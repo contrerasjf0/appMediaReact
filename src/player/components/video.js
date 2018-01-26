84d4c0eb4c56f5f778a440c4b5f667028408205f
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-01-26 06:24:36 -0600

    It was added the spinner for the delay that show of the video when it is control the time in the bar

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -2,29 +2,29 @@ import React, { Component } from 'react';
 import './video.css';
 
 class Video extends Component {
-    togglePlay() {
-        if (this.props.pause) {
-        this.video.play()
-        } else {
-        this.video.pause()
-        }
+  togglePlay() {
+    if (this.props.pause) {
+      this.video.play()
+    } else {
+      this.video.pause()
     }
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.pause !== this.props.pause) {
-        this.togglePlay();
-        }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pause !== this.props.pause) {
+      this.togglePlay();
     }
-    setRef = element => {
-        this.video = element;
-    }
+  }
+  setRef = element => {
+    this.video = element;
+  }
   render() {
-
     const {
-        handleLoadedMetadata,
-        handleTimeUpdate,
-      } = this.props;
+      handleLoadedMetadata,
+      handleTimeUpdate,
+      handleSeeking,
+      handleSeeked,
+    } = this.props;
 
-      
     return (
       <div className="Video">
         <video
@@ -33,6 +33,8 @@ class Video extends Component {
           ref={this.setRef}
           onLoadedMetadata={handleLoadedMetadata}
           onTimeUpdate={handleTimeUpdate}
+          onSeeking={handleSeeking}
+          onSeeked={handleSeeked}
         />
       </div>
     )
